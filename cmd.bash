987158ce74ca7688693a6cf6868ce2340f533df0
Oscar Lima <oscar.lima@dfki.de>
2025-09-25 11:53:23 +0200

    update docker to be useful in Ubuntu 24.04

--- cmd.bash
+++ cmd.bash
@@ -2,5 +2,5 @@
 # run single command
 xhost +local:root
 # run the robotics parts of the framework
-docker-compose run mobipick_cmd $@
+docker compose run mobipick_cmd $@
 xhost -local:root
--- mobipick.bash
+++ mobipick.bash
@@ -3,5 +3,5 @@ docker network create mobipick
 # copied from marlin
 xhost +local:root
 # run the robotics parts of the framework
-docker-compose run mobipick
+docker compose run mobipick
 xhost -local:root
